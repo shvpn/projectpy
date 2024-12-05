8255b06fea8652b041ce5d2f7135e4586db343d6
shvpn <oukoksa11@gmail.com>
2024-12-05 20:14:48 +0700

    more

---
+++ filemsg.py
@@ -0,0 +1,28 @@
+import customtkinter as ctk
+from tkinter import messagebox
+
+
+class filemsg:
+    def __init__(self):
+        self.input_path = ""
+        self.msg = ""
+    def choose_msg_from_file(self):
+        root = ctk.CTk()
+        try:
+            root.withdraw() # Hide the main window
+            self.input_path = ctk.filedialog.askopenfilename(
+            title="Select an input file",
+            filetypes=(("Text files", "*.txt"), ("Config Files", "*.conf;*.json"), ("All files", "*.*"))
+        )
+            root.destroy() # Close the window
+            if self.input_path:
+                with open(self.input_path, "r") as file:
+                    self.msg = file.read()
+            else:
+                raise ValueError("No file selected")
+        except Exception as e:
+            return "File Not Loaded"
+            messagebox.showerror("File Error", str(e))
+        else:
+            return self.msg
+            messagebox.showinfo("File Loaded", "Message loaded successfully!")
